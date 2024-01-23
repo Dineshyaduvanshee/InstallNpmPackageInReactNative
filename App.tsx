ff982b35020aff91727132682d01cfafef8c84ab
Dinesh yadav <dineshyadav74950@gmail.com>
2024-01-23 18:48:59 +0530

    Install NPM Package in React-native =>what is Packages =>How to install Packages =>Write code to apply changes =>Interview

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View,Text } from "react-native";
+import React  from "react";
+import WebView, {} from "react-native-webview";
+const App =() =>{
+  return(
+  // <WebView source={{uri:"https://reactnative.dev/"}} />
+  <WebView source={{uri:"https://www.indicosmic.com/index.html"}}/>
+    )
+}
+export default App;
